0f1c22366885658a8d266f1da71418b7d41cc654
Derek Selander <derekselander@yahoo.com>
2018-02-18 17:46:29 -0700

    Add much cool color wow to list_applications

--- mobdevim/List Applications/list_applications.m
+++ mobdevim/List Applications/list_applications.m
@@ -14,10 +14,10 @@ NSString *const kListApplicationsKey = @"com.selander.listapplications.key";
 int list_applications(AMDeviceRef d, NSDictionary *options) {
   
   NSDictionary *dict;
-  AMDeviceLookupApplications(d, @{@"ReturnAttributes": @YES}, &dict);
-  
   NSString *name = [options objectForKey:kListApplicationsName];
+
   if (name) {
+    AMDeviceLookupApplications(d, @{ @"ReturnAttributes": @YES, @"ShowLaunchProhibitedApps" : @YES }, &dict);
     if (![dict objectForKey:name]) {
       dsprintf(stderr, "%sCouldn't find the bundleIdentifier \"%s\", try listing all bundleIDs with %s%smobdevim -l%s\n", dcolor("yellow"), [name UTF8String], colorEnd(), dcolor("bold"), colorEnd());
       return 1;
@@ -34,12 +34,30 @@ int list_applications(AMDeviceRef d, NSDictionary *options) {
           dsprintf(stdout, "%sDumping info for \"%s\"%s\n\n%s\n", dcolor("red"), [name UTF8String], colorEnd(),  [[[dict objectForKey:name] debugDescription] UTF8String]);
       }
   } else {
+    // name is nil
+    AMDeviceLookupApplications(d, @{@"ReturnAttributes": @[@"ProfileValidated", @"CFBundleIdentifier", @"SBAppTags", @"ApplicationType"], @"ShowLaunchProhibitedApps" : @YES}, &dict);
     NSMutableString *output = [NSMutableString string];
     for (NSString *key in [dict allKeys]) {
-      [output appendString:[NSString stringWithFormat:@"%@\n", key]];
+      NSDictionary *appDict = dict[key];
+      if ([appDict[@"ProfileValidated"] boolValue]) {
+        [output appendString:[NSString stringWithFormat:@"%s%@%s\n", dcolor("cyan"), key, colorEnd()]];
+      } else if ([appDict[@"ApplicationType"] isEqualToString:@"Internal"]) {
+        [output appendString:[NSString stringWithFormat:@"%s%@%s\n", dcolor("magenta"), key, colorEnd()]];
+      } else if ([appDict[@"SBAppTags"] containsObject:@"hidden"]) {
+        [output appendString:[NSString stringWithFormat:@"%s%@%s\n", dcolor("yellow"), key, colorEnd()]];
+    } else if ([appDict[@"ApplicationType"] isEqualToString:@"System"]) {
+      [output appendString:[NSString stringWithFormat:@"%s%@%s\n", dcolor("red"), key, colorEnd()]];
+    }
+      else {
+        [output appendString:[NSString stringWithFormat:@"%@\n", key]];
+      }
     }
     
-    dsprintf(stdout, "%sDumping bundleIDs for all apps%s\n\n%s\n\n", dcolor("red"), colorEnd(), [output UTF8String]);
+    NSString *colorHelper = @"";
+    if (getenv("DSCOLOR")) {
+      colorHelper = [NSString stringWithFormat:@"\t%s█ Hidden%s\t%s█ Developer%s\t%s█ System%s\t%s█ Internal%s\n", dcolor("yellow"), colorEnd(), dcolor("cyan"), colorEnd(), dcolor("red"), colorEnd(), dcolor("magenta"), colorEnd()];
+    }
+    dsprintf(stdout, "Dumping bundleIDs for all apps\n\n%s%s\n\n", [colorHelper UTF8String],  [output UTF8String]);
   }
   
   return 0;
